185ef8ad57455955887598d989d5105a8c78ed19
kylethibbs <kthibbs@gmail.com>
2022-03-13 03:53:48 -0400

    Create Board

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace fifteen
 {
-    partial class Form1
+    partial class form_Fifteen
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,240 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_start_game = new System.Windows.Forms.Button();
+            this.panel_buttonPanel = new System.Windows.Forms.Panel();
+            this.btn_16 = new System.Windows.Forms.Button();
+            this.btn_15 = new System.Windows.Forms.Button();
+            this.btn_14 = new System.Windows.Forms.Button();
+            this.btn_13 = new System.Windows.Forms.Button();
+            this.btn_12 = new System.Windows.Forms.Button();
+            this.btn_11 = new System.Windows.Forms.Button();
+            this.btn_10 = new System.Windows.Forms.Button();
+            this.btn_9 = new System.Windows.Forms.Button();
+            this.btn_8 = new System.Windows.Forms.Button();
+            this.btn_7 = new System.Windows.Forms.Button();
+            this.btn_6 = new System.Windows.Forms.Button();
+            this.btn_5 = new System.Windows.Forms.Button();
+            this.btn_4 = new System.Windows.Forms.Button();
+            this.btn_3 = new System.Windows.Forms.Button();
+            this.btn_2 = new System.Windows.Forms.Button();
+            this.btn_1 = new System.Windows.Forms.Button();
+            this.panel_buttonPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_start_game
+            // 
+            this.btn_start_game.Location = new System.Drawing.Point(90, 101);
+            this.btn_start_game.Name = "btn_start_game";
+            this.btn_start_game.Size = new System.Drawing.Size(208, 25);
+            this.btn_start_game.TabIndex = 0;
+            this.btn_start_game.Text = "Start Game";
+            this.btn_start_game.UseVisualStyleBackColor = true;
+            this.btn_start_game.Click += new System.EventHandler(this.btn_start_game_Click);
+            // 
+            // panel_buttonPanel
+            // 
+            this.panel_buttonPanel.Controls.Add(this.btn_16);
+            this.panel_buttonPanel.Controls.Add(this.btn_15);
+            this.panel_buttonPanel.Controls.Add(this.btn_14);
+            this.panel_buttonPanel.Controls.Add(this.btn_13);
+            this.panel_buttonPanel.Controls.Add(this.btn_12);
+            this.panel_buttonPanel.Controls.Add(this.btn_11);
+            this.panel_buttonPanel.Controls.Add(this.btn_10);
+            this.panel_buttonPanel.Controls.Add(this.btn_9);
+            this.panel_buttonPanel.Controls.Add(this.btn_8);
+            this.panel_buttonPanel.Controls.Add(this.btn_7);
+            this.panel_buttonPanel.Controls.Add(this.btn_6);
+            this.panel_buttonPanel.Controls.Add(this.btn_5);
+            this.panel_buttonPanel.Controls.Add(this.btn_4);
+            this.panel_buttonPanel.Controls.Add(this.btn_3);
+            this.panel_buttonPanel.Controls.Add(this.btn_2);
+            this.panel_buttonPanel.Controls.Add(this.btn_1);
+            this.panel_buttonPanel.Location = new System.Drawing.Point(12, 132);
+            this.panel_buttonPanel.Name = "panel_buttonPanel";
+            this.panel_buttonPanel.Size = new System.Drawing.Size(360, 263);
+            this.panel_buttonPanel.TabIndex = 1;
+            // 
+            // btn_16
+            // 
+            this.btn_16.Enabled = false;
+            this.btn_16.Location = new System.Drawing.Point(238, 189);
+            this.btn_16.Name = "btn_16";
+            this.btn_16.Size = new System.Drawing.Size(50, 50);
+            this.btn_16.TabIndex = 15;
+            this.btn_16.UseVisualStyleBackColor = true;
+            // 
+            // btn_15
+            // 
+            this.btn_15.Enabled = false;
+            this.btn_15.Location = new System.Drawing.Point(182, 189);
+            this.btn_15.Name = "btn_15";
+            this.btn_15.Size = new System.Drawing.Size(50, 50);
+            this.btn_15.TabIndex = 14;
+            this.btn_15.UseVisualStyleBackColor = true;
+            // 
+            // btn_14
+            // 
+            this.btn_14.Enabled = false;
+            this.btn_14.Location = new System.Drawing.Point(126, 189);
+            this.btn_14.Name = "btn_14";
+            this.btn_14.Size = new System.Drawing.Size(50, 50);
+            this.btn_14.TabIndex = 13;
+            this.btn_14.UseVisualStyleBackColor = true;
+            // 
+            // btn_13
+            // 
+            this.btn_13.Enabled = false;
+            this.btn_13.Location = new System.Drawing.Point(70, 189);
+            this.btn_13.Name = "btn_13";
+            this.btn_13.Size = new System.Drawing.Size(50, 50);
+            this.btn_13.TabIndex = 12;
+            this.btn_13.UseVisualStyleBackColor = true;
+            // 
+            // btn_12
+            // 
+            this.btn_12.Enabled = false;
+            this.btn_12.Location = new System.Drawing.Point(238, 133);
+            this.btn_12.Name = "btn_12";
+            this.btn_12.Size = new System.Drawing.Size(50, 50);
+            this.btn_12.TabIndex = 11;
+            this.btn_12.UseVisualStyleBackColor = true;
+            // 
+            // btn_11
+            // 
+            this.btn_11.Enabled = false;
+            this.btn_11.Location = new System.Drawing.Point(182, 133);
+            this.btn_11.Name = "btn_11";
+            this.btn_11.Size = new System.Drawing.Size(50, 50);
+            this.btn_11.TabIndex = 10;
+            this.btn_11.UseVisualStyleBackColor = true;
+            // 
+            // btn_10
+            // 
+            this.btn_10.Enabled = false;
+            this.btn_10.Location = new System.Drawing.Point(126, 133);
+            this.btn_10.Name = "btn_10";
+            this.btn_10.Size = new System.Drawing.Size(50, 50);
+            this.btn_10.TabIndex = 9;
+            this.btn_10.UseVisualStyleBackColor = true;
+            // 
+            // btn_9
+            // 
+            this.btn_9.Enabled = false;
+            this.btn_9.Location = new System.Drawing.Point(70, 133);
+            this.btn_9.Name = "btn_9";
+            this.btn_9.Size = new System.Drawing.Size(50, 50);
+            this.btn_9.TabIndex = 8;
+            this.btn_9.UseVisualStyleBackColor = true;
+            // 
+            // btn_8
+            // 
+            this.btn_8.Enabled = false;
+            this.btn_8.Location = new System.Drawing.Point(238, 77);
+            this.btn_8.Name = "btn_8";
+            this.btn_8.Size = new System.Drawing.Size(50, 50);
+            this.btn_8.TabIndex = 7;
+            this.btn_8.UseVisualStyleBackColor = true;
+            // 
+            // btn_7
+            // 
+            this.btn_7.Enabled = false;
+            this.btn_7.Location = new System.Drawing.Point(182, 77);
+            this.btn_7.Name = "btn_7";
+            this.btn_7.Size = new System.Drawing.Size(50, 50);
+            this.btn_7.TabIndex = 6;
+            this.btn_7.UseVisualStyleBackColor = true;
+            // 
+            // btn_6
+            // 
+            this.btn_6.Enabled = false;
+            this.btn_6.Location = new System.Drawing.Point(126, 77);
+            this.btn_6.Name = "btn_6";
+            this.btn_6.Size = new System.Drawing.Size(50, 50);
+            this.btn_6.TabIndex = 5;
+            this.btn_6.UseVisualStyleBackColor = true;
+            // 
+            // btn_5
+            // 
+            this.btn_5.Enabled = false;
+            this.btn_5.Location = new System.Drawing.Point(70, 77);
+            this.btn_5.Name = "btn_5";
+            this.btn_5.Size = new System.Drawing.Size(50, 50);
+            this.btn_5.TabIndex = 4;
+            this.btn_5.UseVisualStyleBackColor = true;
+            // 
+            // btn_4
+            // 
+            this.btn_4.Enabled = false;
+            this.btn_4.Location = new System.Drawing.Point(238, 17);
+            this.btn_4.Name = "btn_4";
+            this.btn_4.Size = new System.Drawing.Size(50, 50);
+            this.btn_4.TabIndex = 3;
+            this.btn_4.UseVisualStyleBackColor = true;
+            // 
+            // btn_3
+            // 
+            this.btn_3.Enabled = false;
+            this.btn_3.Location = new System.Drawing.Point(182, 17);
+            this.btn_3.Name = "btn_3";
+            this.btn_3.Size = new System.Drawing.Size(50, 50);
+            this.btn_3.TabIndex = 2;
+            this.btn_3.UseVisualStyleBackColor = true;
+            // 
+            // btn_2
+            // 
+            this.btn_2.Enabled = false;
+            this.btn_2.Location = new System.Drawing.Point(126, 17);
+            this.btn_2.Name = "btn_2";
+            this.btn_2.Size = new System.Drawing.Size(50, 50);
+            this.btn_2.TabIndex = 1;
+            this.btn_2.UseVisualStyleBackColor = true;
+            // 
+            // btn_1
+            // 
+            this.btn_1.Enabled = false;
+            this.btn_1.Location = new System.Drawing.Point(70, 17);
+            this.btn_1.Name = "btn_1";
+            this.btn_1.Size = new System.Drawing.Size(50, 50);
+            this.btn_1.TabIndex = 0;
+            this.btn_1.UseVisualStyleBackColor = true;
+            // 
+            // form_Fifteen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(384, 561);
+            this.Controls.Add(this.panel_buttonPanel);
+            this.Controls.Add(this.btn_start_game);
+            this.Name = "form_Fifteen";
+            this.Text = "fifteen";
+            this.panel_buttonPanel.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button btn_start_game;
+        private Panel panel_buttonPanel;
+        private Button btn_16;
+        private Button btn_15;
+        private Button btn_14;
+        private Button btn_13;
+        private Button btn_12;
+        private Button btn_11;
+        private Button btn_10;
+        private Button btn_9;
+        private Button btn_8;
+        private Button btn_7;
+        private Button btn_6;
+        private Button btn_5;
+        private Button btn_4;
+        private Button btn_3;
+        private Button btn_2;
+        private Button btn_1;
+
+        
     }
 }
